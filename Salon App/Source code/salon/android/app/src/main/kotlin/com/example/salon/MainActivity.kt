8d9dece8a87d2e8999708f769c77c2db9883ef6f
binasad <usman504757@gmail.com>
2026-02-10 12:15:28 +0500

    Fix: convert salon-app submodule to regular folder

--- Salon App/Source code/salon/android/app/src/main/kotlin/com/example/salon/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.salon
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Salon App/Source code/salon/lib/Manager/NotificationManager.dart
+++
@@ -1,8 +0,0 @@
-// lib/Manager/NotificationManager.dart
-class NotificationManager {
-  static List<String> notifications = [];
-
-  static void addNotification(String message) {
-    notifications.add(message);
-  }
-}
